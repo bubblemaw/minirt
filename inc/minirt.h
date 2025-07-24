f07b35cdac3ffe7cd99dba0cfc692fa0e9e089f2
TheOrangeCake <vietfox94@gmail.com>
2025-07-24 11:37:55 +0200

    rendu

--- inc/minirt.h
+++ inc/minirt.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minirt.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: maw <maw@student.42.fr>                    +#+  +:+       +#+        */
+/*   By: hoannguy <hoannguy@student.42lausanne.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/06 13:05:52 by masase            #+#    #+#             */
-/*   Updated: 2025/07/17 17:50:08 by maw              ###   ########.fr       */
+/*   Updated: 2025/07/24 11:28:33 by hoannguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,8 +52,8 @@
 # define XPM_HEIGHT 500
 # define XPM_WIDTH 1000
 
-# define HEIGHT 720
-# define WIDTH 1080
+# define HEIGHT 1080
+# define WIDTH 1920
 
 typedef enum t_tab_type
 {
